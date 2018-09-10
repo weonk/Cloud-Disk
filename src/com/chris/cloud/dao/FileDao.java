441bdffbbeec762c10ccc76fedf403ac7578ef27
吴清 <37136688+iamchriswu@users.noreply.github.com>
2018-09-10 21:52:07 +0800

    Add files via upload
    改了点接口

--- src/com/chris/cloud/dao/FileDao.java
+++ src/com/chris/cloud/dao/FileDao.java
@@ -1,4 +1,13 @@
 package com.chris.cloud.dao;
 
+import com.chris.cloud.entity.FileList;
+
+import java.util.List;
+
 public interface FileDao {
+    int insertFile(FileList fileList) throws Exception;
+    int deleteFile(int id) throws Exception;
+    FileList getOneFileByIndex(int id) throws Exception;
+    List<FileList> getFilesByUserId(int userId) throws Exception;
+    List<FileList> getFilesByKey(String key) throws Exception;
 }
--- src/com/chris/cloud/dao/LogDao.java
+++ src/com/chris/cloud/dao/LogDao.java
@@ -1,4 +1,7 @@
 package com.chris.cloud.dao;
 
+import com.chris.cloud.entity.Log;
+
 public interface LogDao {
+    int insertLog(Log log) throws Exception;
 }
